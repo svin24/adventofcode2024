21c9d9d16b04b061a2c2616902e58ed631c0a133
svin24 <svin.mail.24@gmail.com>
2024-12-03 09:24:19 +0200

    day 3

---
+++ day3/day3.go
@@ -0,0 +1,55 @@
+package day3
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func Day3() {
+	file, err := os.ReadFile("./day3/input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	//part 1
+	regex := regexp.MustCompile(`mul\([0-9]+,[0-9]+\)`)
+	matchMul := regex.FindAllString(string(file), -1)
+	fmt.Println("Part 1:", trimAndCalculate(matchMul))
+
+	//part 2
+	regex = regexp.MustCompile(`(do\(\)|don't\(\)|mul\(\d+,\d+\))`)
+	matchMul = regex.FindAllString(string(file), -1)
+
+	var result []string
+	isEnabled := true
+	for _, match := range matchMul {
+		if match == "do()" {
+			isEnabled = true
+		} else if match == "don't()" {
+			isEnabled = false
+		} else if isEnabled && match[:4] == "mul(" {
+			result = append(result, match)
+		}
+	}
+
+	fmt.Println("Part 2:", trimAndCalculate(result))
+}
+
+func trimAndCalculate(data []string) int {
+	var numParts []int
+	var sum int = 0
+	for i := range data {
+		trim := strings.TrimSuffix(strings.TrimPrefix(data[i], "mul("), ")")
+		parts := strings.Split(trim, ",")
+		num0, _ := strconv.Atoi(parts[0])
+		num1, _ := strconv.Atoi(parts[1])
+		numParts = append(numParts, num0, num1)
+	}
+	for i := 0; i < len(numParts); i += 2 {
+		sum = numParts[i]*numParts[i+1] + sum
+	}
+	return sum
+}
--- main.go
+++ main.go
@@ -1,10 +1,7 @@
 package main
 
-import "adventofcode2024/day2"
-
-//import "adventofcode2024/day1"
+import "adventofcode2024/day3"
 
 func main() {
-	//day1.Day1()
-	day2.Day2()
+	day3.Day3()
 }

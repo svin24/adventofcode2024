bf304f33382918b0ed92171efa207766bd70e101
svin24 <svin.mail.24@gmail.com>
2024-12-04 13:09:29 +0200

    day 4 part 2

--- day4/day4.go
+++ day4/day4.go
@@ -27,30 +27,74 @@ func Day4() {
 		{1, -1},  // Diagonal Down-Left
 	}
 
-	var count int = 0
+	var silver int = 0
+	var gold int = 0
 	for r := range matrix {
 		for c := range matrix[0] {
 			for _, dir := range directions {
-				var check bool = true
-				for i := range word {
-					newRow := r + i*dir[0]
-					newCol := c + i*dir[1]
-					if newRow < 0 || newRow >= len(matrix) || newCol < 0 || newCol >= len(matrix[0]) {
-						check = false
-						continue
-					}
-					if matrix[newRow][newCol] != string(word[i]) {
-						check = false
-						continue
-					}
-				}
-				if check {
-					count++
+				if findXmasPart1(matrix, r, c, word, dir) {
+					silver++
 				}
 			}
 		}
 	}
-	fmt.Println(count)
+	// part 2
+	diagonals := [][2]int{
+		{1, 1},   // Diagonal Down-Right
+		{-1, 1},  // Diagonal Up-Right
+		{-1, -1}, // Diagonal Up-Left
+		{1, -1},  // Diagonal Down-Left
+	}
+	for r := range matrix {
+		for c := range matrix[0] {
+			//part 2
+			if matrix[r][c] == "A" {
+				if findXmasPart2(matrix, r, c, diagonals) {
+					gold++
+				}
+			}
+
+		}
+	}
+	fmt.Println("Part 1:", silver)
+	fmt.Println("Part 2:", gold)
+}
+
+func findXmasPart1(matrix [][]string, row int, col int, word string, dir [2]int) bool {
+	for i := range word {
+		newRow := row + i*dir[0]
+		newCol := col + i*dir[1]
+		//check the limits
+		if newRow < 0 || newRow >= len(matrix) || newCol < 0 || newCol >= len(matrix[0]) {
+			return false
+		}
+		if matrix[newRow][newCol] != string(word[i]) {
+			return false
+		}
+	}
+	return true
+}
+
+func findXmasPart2(matrix [][]string, row int, col int, diag [][2]int) bool {
+	// each MAS should have an A in the middle
+	mCount := 0
+	for _, d := range diag {
+		newRow := row + d[0]
+		newCol := col + d[1]
+		opRow := row + d[0]*-1
+		opCol := col + d[1]*-1
+		if newRow < 0 || newRow >= len(matrix) || newCol < 0 || newCol >= len(matrix[0]) || opRow < 0 || opRow >= len(matrix) || opCol < 0 || opCol >= len(matrix[0]) {
+			return false
+		}
+		if matrix[newRow][newCol] == "M" && matrix[opRow][opCol] == "S" {
+			mCount++
+			if mCount > 2 {
+				return false
+			}
+		}
+	}
+
+	return mCount == 2
 }
 
 func readXmasFile(filename string) ([][]string, error) {
